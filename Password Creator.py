f0a501977d6bc52a80b825bcd109535b1cdcdc99
chitkarshm <chitkarshm@gmail.com>
2025-06-15 11:09:32 +0530

    more games

--- Password Creator.py
+++ Password Creator.py
@@ -1,7 +1,7 @@
 password_pone = input(
     "type in a meaningful word with uppercase and lowercase letters.\nEg:ChOClAtE"
 )
-password_ptwo = input("type in four or less numbers.\nEg:5481")
+password_ptwo = input("type in 4 numbers.\nEg:5481")
 password_pthree = input("type in a special character.\nEg:@")
 password_pfour = input("type in a word with uppercase and lowercase letters.\nEg:LiFT")
 u = password_pone + password_ptwo + password_pthree + password_pfour
