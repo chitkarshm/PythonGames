f98a693442c6c6d540f34006c80a8ddf5e330046
chitkarshm <chitkarshm@gmail.com>
2025-06-21 14:06:05 +0530

    2 die support
    now you can choose 1 or 2 die for different board games!
    PIN  maker is just reformatted, no changes.

--- Dice.py
+++ Dice.py
@@ -1,8 +1,27 @@
 import random
-while True:
-      roll = input("do you want to roll a die?")
-      if roll == "yes" or roll == "y":
-            dice = random.randint(1, 6)
-            print("You rolled a " + str(dice) + "!")
-      else:
+
+num_of_die = input("do you want to roll 1 dice or 2?")
+if num_of_die == "1":
+    while True:
+        dice = random.randint(1, 6)
+        print(f"You rolled a {dice}!")
+        redo = input("do you want to continue?")
+        if redo == "yes" or redo == "y":
+            continue
+        else:
+            print("OK, bye!")
             break
+elif num_of_die == "2":
+    while True:
+        dicea = random.randint(1, 6)
+        diceb = random.randint(1, 6)
+        total = dicea + diceb
+        print(f"You rolled a {total}!")
+        redo = input("do you want to continue?")
+        if redo == "yes" or redo == "y":
+            continue
+        else:
+            print("OK, bye!")
+            break
+else:
+    print("Please enter only 1 or 2.Bye!")
--- PIN Maker.py
+++ PIN Maker.py
@@ -1,6 +1,7 @@
 import random
-pin=""
+
+pin = ""
 while len(pin) < 4:
-      digit = random.randint(0,9)
-      pin = pin + str(digit)
-print("Your PIN is " + pin )
+    digit = random.randint(0, 9)
+    pin = pin + str(digit)
+print("Your PIN is " + pin)
