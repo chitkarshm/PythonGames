f95c22e827cff909d6ff1d5e1a4e75d610370249
chitkarshm <chitkarshm@gmail.com>
2025-06-21 14:46:01 +0530

    dedup continue prompt
    created new function to dedup user prompt.

--- Dice.py
+++ Dice.py
@@ -1,15 +1,21 @@
 import random
 
-num_of_die = input("do you want to roll 1 dice or 2?")
+
+def ask_continue():
+    redo = input("Do you want to continue?")
+    if redo == "yes" or redo == "y":
+        return True
+    else:
+        print("OK, bye!")
+        return False
+
+
+num_of_die = input("Do you want to roll 1 dice or 2?")
 if num_of_die == "1":
     while True:
         dice = random.randint(1, 6)
         print(f"You rolled a {dice}!")
-        redo = input("do you want to continue?")
-        if redo == "yes" or redo == "y":
-            continue
-        else:
-            print("OK, bye!")
+        if not ask_continue():
             break
 elif num_of_die == "2":
     while True:
@@ -17,11 +23,7 @@ elif num_of_die == "2":
         diceb = random.randint(1, 6)
         total = dicea + diceb
         print(f"You rolled a {total}!")
-        redo = input("do you want to continue?")
-        if redo == "yes" or redo == "y":
-            continue
-        else:
-            print("OK, bye!")
+        if not ask_continue():
             break
 else:
     print("Please enter only 1 or 2.Bye!")
