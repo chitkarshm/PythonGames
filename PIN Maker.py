70494229d308d8ef351b309fa2bb256536248a37
chitkarshm <chitkarshm@gmail.com>
2025-06-25 15:30:24 +0530

    deleted something and made pin maker better

--- PIN Maker.py
+++ PIN Maker.py
@@ -1,7 +1,18 @@
 import random
+def make_PIN():
+      PIN = ""
+      while len(PIN) < 4:
+        digit = random.randint(0, 9)
+        PIN = PIN + str(digit)
+      print(f"Your PIN is {PIN} ")
 
-pin = ""
-while len(pin) < 4:
-    digit = random.randint(0, 9)
-    pin = pin + str(digit)
-print("Your PIN is " + pin)
+def change_and_repeat():
+    changePIN=input("Do you want to change your PIN?")
+    while changePIN != "no":
+        make_PIN()
+        changePIN=input("Do you want to change your PIN?")
+        if changePIN == "no":
+            print("OK, bye!")
+            break
+make_PIN()
+change_and_repeat()
